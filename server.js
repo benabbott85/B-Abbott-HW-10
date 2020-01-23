7cb540a861493d7dd41f9d38da22c740ed0903d0
Ben Abbott <babbott85@gmail.com>
2020-01-23 15:58:27 -0700

    post functionality added to server file

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var path = require("path");
+var fs = require ("fs");
 
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+var newnote= [];
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -15,6 +18,16 @@ app.get("/", function(req, res){
 app.get("/notes", function (req, res){
     res.sendFile(path.join(__dirname, "notes.html"))
 });
+
+app.post("/notes", function (req, res){
+   var addnote= req.body;
+   console.log(addnote);
+
+   newnote.push(addnote);
+
+   res.json(addnote);
+
+})
 
 app.listen(PORT, function(){
     console.log("App listening on PORT" + PORT);
